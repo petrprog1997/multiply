6572fbfdb1c45fbe03032b1aeff47de6a6feca64
petrprog1997 <petrprog1997@mail.ru>
2019-09-30 18:22:17 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function multiply(first, second) {
-  // your solution
+ module.exports = 
+function multiply(a, b) {
+  //your solution    
+  let aArray = a.split('').reverse();
+  let bArray = b.split('').reverse();
+  
+
+  let resultArray = [];
+  
+  for(let i = 0; i < aArray.length; i++) {
+  
+    for(let j = 0; j < bArray.length;  j++) {
+      
+      let mult = bArray[j]*aArray[i];
+      
+      resultArray[i+j] = (resultArray[i+j]) ? resultArray[i+j] + mult : mult ;      
+    }
+  }
+
+
+  for(let i = 0; i<resultArray.length; i++){
+    
+    let num  = resultArray[i]%10;
+    
+    let move = Math.floor(resultArray[i]/10);
+    
+    resultArray[i] = num;
+    
+    if(resultArray[i+1]){
+      resultArray[i+1] += move;
+    }
+    
+    else if (move!=0){
+      resultArray[i+1] = move;
+    }
+  }
+  
+  return resultArray.reverse().join('');
 }
